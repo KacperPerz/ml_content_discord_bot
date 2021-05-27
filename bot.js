ef686ff5b9723fb7c5f4f625bb3d5dd6bf40af26
Kacper Perz <kicper2000@gmail.com>
2021-05-27 13:06:29 +0200

    initialize commit

---
+++ bot.js
@@ -0,0 +1,54 @@
+const Discord = require('discord.js');
+const request = require('request');
+const client = new Discord.Client();
+const got = require('got');
+const cheerio = require('cheerio');
+client.login('ODQ3MTc5NTg1NTYwNzcyNjU4.YK6TeQ.NImunzIA1Q4DQVvWNOH8Dg9DP0U');
+
+client.on('ready', readyDiscord);
+
+function readyDiscord() {
+    console.log('ops');
+}
+
+replies = [
+    'aye!',
+    'whazza',
+    'relax'
+]
+
+// Kindacode.com
+const URL = 'https://paperswithcode.com/';
+pwc = new Set();
+request(URL, (error, response, html) => {
+    const $ = cheerio.load(html);
+
+    const divs = $('.item-content');
+    const out = divs.find('h1').find('a').attr('href');
+    const ac = $('a');
+    links = []
+    re = new RegExp('.paper/.*');
+    ac.each((index, element) => {
+        if(re.test($(element).attr('href')))
+        links.push(
+            $(element).attr('href')
+        );
+    });
+    pwc = [... new Set(links)]
+    console.log(pwc);
+});
+
+client.on('message', gotMessage);
+
+function gotMessage(msg) {
+    console.log(msg.content);
+    if(msg.channel.id == '847181337910968370' && msg.content == 'pwc') {
+
+
+        msg.channel.send("Have you seen new paper?\n");
+        msg.channel.send('https://www.paperswithcode.com' + pwc[0]);
+        
+        
+        //console.log(links);
+    }
+}
